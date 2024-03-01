ad668a4cb377e7a6b1182e7104d0174e98ef860a
Alazar351 <alazar.lema351@gmail.com>
2024-03-01 07:45:52 -0600

    Start mobile menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import { ModeToggle } from "./ui/toggleMode";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export default function Nav() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   return (
-    <div className="flex items-center justify-between px-6 py-4">
+    <nav className="flex items-center justify-between px-6 py-4">
       <div className="flex gap-4">
         <Image
           className="rounded"
@@ -21,20 +25,23 @@ export default function Nav() {
       </div>
       <div className="flex gap-8">
         <ModeToggle />
-        <Button
-          className=" rounded-md hover:bg-gray-100 hover:text-gray-400 "
-          variant="ghost"
-          size="icon"
-        >
-          <Menu color="#9CA3AF" strokeWidth={1} size={24} />
-        </Button>
 
-        <nav className="hidden">
-          <ul>
-            <li></li>
-          </ul>
-        </nav>
+        {isDesktop ? (
+          <div>Desktop</div>
+        ) : (
+          <div>
+            <div>
+              <Button
+                className=" rounded-md hover:bg-gray-100 hover:text-gray-400 "
+                variant="ghost"
+                size="icon"
+              >
+                <Menu strokeWidth={1} size={24} />
+              </Button>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </nav>
   );
 }
