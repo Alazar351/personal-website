e2127c651315e9f4a4d804234261a4d3455c3c89
Alazar351 <alazar.lema351@gmail.com>
2024-04-28 20:05:13 -0500

    Services done

---
+++ app/About/components/Service.tsx
@@ -0,0 +1,41 @@
+export default function Service() {
+  return (
+    <section>
+      <h2 className="text-[26px] font-medium">Services</h2>
+      <div className="mt-8 grid w-full grid-cols-1 gap-1 md:grid-cols-2">
+        <div className="flex items-center justify-start gap-4  rounded-t-xl bg-[#efeff0] p-5 dark:bg-[#282828] md:rounded-tl-xl md:rounded-tr-none">
+          <p id="tools" className="text-lg font-medium">
+            Frontend Development
+          </p>
+        </div>
+        <div className="flex items-center justify-start gap-4  bg-[#efeff0] p-5  dark:bg-[#282828] md:rounded-tr-xl">
+          <p id="tools" className="text-lg font-medium">
+            E-Commerce Development
+          </p>
+        </div>
+        {/* Add more services here v */}
+        <div className="flex items-center justify-start gap-4  bg-[#efeff0] p-5  dark:bg-[#282828] ">
+          <p id="tools" className="text-lg font-medium">
+            Responsive Design
+          </p>
+        </div>
+        <div className="flex items-center justify-start gap-4  bg-[#efeff0] p-5  dark:bg-[#282828] ">
+          <p id="tools" className="text-lg font-medium">
+            Maintenance and Support
+          </p>
+        </div>
+        {/* Add more services here ^ */}
+        <div className="flex items-center justify-start gap-4  bg-[#efeff0] p-5  dark:bg-[#282828] md:rounded-bl-xl">
+          <p id="tools" className="text-lg font-medium">
+            API Integration
+          </p>
+        </div>
+        <div className="flex items-center justify-start gap-4 rounded-b-xl bg-[#efeff0] p-5  dark:bg-[#282828] md:rounded-bl-none  md:rounded-br-xl">
+          <p id="tools" className="text-lg font-medium">
+            Content Management Systems(CMS)
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "./components/Intro";
+import Service from "./components/Service";
 
 export default function About() {
   return (
-    <>
+    <div className="space-y-20">
       <Intro />
-    </>
+      <Service />
+    </div>
   );
 }
