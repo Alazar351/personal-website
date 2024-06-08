c2b0a573e044e9182eaa02c9eb9f58f950be6b94
Alazar351 <alazar.lema351@gmail.com>
2024-06-08 15:08:03 -0500

    Calendly fix

--- app/Contact/components/Calendly.tsx
+++ app/Contact/components/Calendly.tsx
@@ -1,18 +1,19 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PopupButton } from "react-calendly";
 
 export default function Calendly() {
+  const [root, setRoot] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setRoot(document.getElementById("root"));
+  }, []);
   return (
     <Button asChild>
       <PopupButton
         url="https://calendly.com/alazar-lema351/quick-call"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
-        rootElement={document.body}
+        rootElement={root}
         text="Schedule Call"
         className="w-full transition-colors duration-700 ease-out dark:bg-[#282828] dark:text-white dark:hover:bg-[#424242] md:max-w-36"
       />
--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -3,9 +3,9 @@ import Message from "./components/Message";
 
 export default function Contact() {
   return (
-    <>
+    <div id="root">
       <Info />
       <Message />
-    </>
+    </div>
   );
 }
