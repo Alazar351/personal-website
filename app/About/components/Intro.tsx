436c77309226fd37a5251e320a9ff4760296a0b9
Alazar351 <alazar.lema351@gmail.com>
2025-07-17 18:32:07 -0500

    up update

--- app/About/components/Intro.tsx
+++ app/About/components/Intro.tsx
@@ -12,13 +12,17 @@ export default function Intro() {
         <p className="mt-10 text-lg text-muted-foreground">
           Hey! I’m a frontend developer in Minnesota who’s passionate about
           building beautiful, responsive websites with modern tools like
-          Next.js, React, and Tailwind CSS. I’m especially into the visual side
-          of development — translating design mockups into real, interactive
-          experiences that look great and feel smooth to use. I’ve spent the
-          last couple years leveling up through personal projects, client work,
-          and a lot of trial and error. Right now, I’m looking to join a team
-          where I can keep learning, contribute to real-world products, and grow
-          into a stronger developer.
+          Next.js, React, and Tailwind CSS.
+        </p>
+        <p className="mt-5 text-lg text-muted-foreground">
+          I’m especially into the visual side of development — translating
+          design mockups into real, interactive experiences that look great and
+          feel smooth to use. I’ve spent the last couple years leveling up
+          through personal projects, client work, and a lot of trial and error.
+        </p>
+        <p className="mt-5 text-lg text-muted-foreground">
+          Right now, I’m looking to join a team where I can keep learning,
+          contribute to real-world products, and grow into a stronger developer.
         </p>
       </div>
       <Image
